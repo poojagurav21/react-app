d5602c1dec9bfb77acb9eba6ed7241a62c89e150
Pooja Gurav <pooja.gurav@waiin.com>
2024-05-28 10:36:14 +0530

    About component dark mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,16 +8,58 @@ export default function TextForm(props) {
     setText(newText);
   };
 
-    const handleLwClick = () => {
+  const handleLwClick = () => {
     // console.log("Uppercase was clicked " +text);
     let newText = text.toLowerCase();
     setText(newText);
-    }
-
+  };
+  const handleClearClick = () => {
+    setText("");
+  };
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
   };
+
+  const handleRevStrClick = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+  };
+
+  const handleCamClick = () => {
+    let newText = text
+      .split(" ")
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+    setText(newText);
+  };
+
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  }
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  
+  }
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+//logic for inverse/reverce string
+//   const handleinverseclick = () => {
+//     console.log("inverse click is triggered");
+//     let newtext = "";
+//     for (let i = text.length - 1; i >= 0; i--) {
+//       newtext += text[i];
+//     }
+//     setText(newtext);
+//   };
 
   const [text, setText] = useState("");
 
@@ -34,17 +76,40 @@ export default function TextForm(props) {
             rows="10"
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-success mx-3" onClick={handleLwClick}>
+        <button className="btn btn-primary mx-2" onClick={handleLwClick}>
           Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCamClick}>
+          Convert To Cammel case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleRevStrClick}>
+          Reverse the String
+        </button>
+        <button className="btn btn-primary mx-2" onClick={speak} >
+          Speak
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy} >
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces} >
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-warning mx-2" onClick={handleClearClick}>
+          Clear Text
         </button>
       </div>
       <div className="container my-3">
         <h2>Your Text Summary</h2>
-        <p><b>{text.split(" ").length}</b> words and <b>{text.length}</b> Characters</p>
-        <p><b>{0.008*text.split(" ").length}</b> Minutes read time</p>
+        <p>
+          <b>{text.split(" ").length}</b> words and <b>{text.length}</b>{" "}
+          Characters
+        </p>
+        <p>
+          <b>{0.008 * text.split(" ").length}</b> Minutes read time
+        </p>
         <h3>Preview</h3>
         <p>{text}</p>
       </div>
